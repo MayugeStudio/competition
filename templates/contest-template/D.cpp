077bcbc4c0206b1b236782dff9a0eb662825db93
kawahara <lost.of.milk1@gmail.com>
2025-12-10 11:40:41 +0900

    Create Templates!

---
+++ templates/contest-template/D.cpp
@@ -0,0 +1,24 @@
+// Headers
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type definitions
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+
+// Macros
+#define rep(i, x, limit) for (int i=(int)x; (int)i<limit; ++i)
+#define all(x)  x.begin(),  x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define Yes cout << "Yes" << endl;
+#define No  cout << "No" << endl;
+#define YES cout << "YES" << endl;
+#define NO  cout << "NO" << endl;
+
+// Entry Point
+int main() {
+  return 0;
+}
+
