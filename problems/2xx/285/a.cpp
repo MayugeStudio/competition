600918f0d9ee44ddf780873ec64aa32974736f2e
kawadada <72333443+MayugeStudio@users.noreply.github.com>
2026-01-06 10:58:06 +0900

    1/06 problems

---
+++ problems/2xx/285/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b; cin >> a >> b;
+  if (2*a == b || 2*a+1 == b) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
+
