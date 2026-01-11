cbc9c0f9d55745a101f631049f9b1fe1118ebc2c
kawadada <72333443+MayugeStudio@users.noreply.github.com>
2026-01-11 21:51:36 +0900

    Introduce init.py

---
+++ init.py
@@ -0,0 +1,7 @@
+import os
+
+if len(sys.argv) == 1:
+    print("ERROR: no argument is provided")
+    sys.exit(1)
+os.system(f"cp ./templates/template.cpp {sys.argv[1]}")
+print(f"copying template.cpp into {sys.argv[1]}")
