fae2a6d338b3838934a12ee2cfba48c27e0578d0
Kawahara <My.favorite.is.energy@gmail.com>
2026-03-19 18:54:47 +0900

    Delete init.py

--- init.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-if len(sys.argv) == 1:
-    print("ERROR: no argument is provided")
-    sys.exit(1)
-os.system(f"cp ./templates/template.cpp {sys.argv[1]}")
-print(f"copying template.cpp into {sys.argv[1]}")
