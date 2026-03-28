88bd93ef27164000333509fbad95f6019f93d302
Kawahara <My.favorite.is.energy@gmail.com>
2026-03-28 21:07:27 +0900

    tenkei90 5/90

---
+++ tenkei90/14.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n; cin >> n;
+  vector<ll> a(n), b(n);
+  for (int i=0; i<n; ++i) cin >> a[i];
+  for (int i=0; i<n; ++i) cin >> b[i];
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  ll ans = 0;
+  for (int i=0; i<n; ++i) {
+    ans += abs(a[i] - b[i]);
+  }
+  cout << ans << endl;
+
+  return 0;
+}
